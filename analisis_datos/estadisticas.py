64c85b6e9a05387414ab5bf8bee7bc9ad150a9e6
Adriana0993 <adrialrod28@gmail.com>
2025-11-21 14:45:23 +0100

    clase 11 corrección

--- analisis_datos/estadisticas.py
+++ analisis_datos/estadisticas.py
@@ -7,7 +7,6 @@ def media(datos: List[Union[int, float]]) -> float:
     Debe lanzar un ValueError si la lista está vacía.
     """
     # 1. Valida si la lista 'datos' está vacía. Si sí, lanza ValueError.
-    # TU CÓDIGO AQUÍ
     if not datos:
         raise ValueError('La lista de datos no puede estar vacìa para calcular el promedio')
     # 2. Retorna la suma dividida por la cantidad de elementos
@@ -27,14 +26,12 @@ def mediana(datos: List[Union[int, float]]) -> Union[int, float]:
     # TU CÓDIGO AQUÍ
 datos_sorted = sorted(datos)
 
-    n = len(datos)
-    mitad = n // 2
+n = len(datos)
+mitad = n // 2
 
     # 2. Si n es par, devuelve el promedio de los dos del centro
     # 3. Si n es impar, devuelve el elemento central
-    if n % 2 == 0:
-        # TU CÓDIGO AQUÍ
-        return(datos_sorted[mitad - 1] + datos_sorted[mitad]) / 2
-    else:
-        # TU CÓDIGO AQUÍ
-        return datos_sorted[mitad]
+if n % 2 == 0:
+    return(datos_sorted[mitad - 1] + datos_sorted[mitad]) / 2
+else:
+    return datos_sorted[mitad]
